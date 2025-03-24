d874094ffd6506472abe2997e42e32afa630398f
Hugo Chavar <hechavar@gmail.com>
2025-03-24 14:08:39 -0300

    Configure the WebSocket Endpoint

--- lib/ws_server_web/endpoint.ex
+++ lib/ws_server_web/endpoint.ex
@@ -15,7 +15,12 @@ defmodule WsServerWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
-  # Serve at "/" the static files from "priv/static" directory.
+
+  socket "/socket", WsServerWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
+    # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
