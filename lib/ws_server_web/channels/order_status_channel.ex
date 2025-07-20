bd288595491ef0770e4463e7bf1a9a0453739c0e
Hugo Chavar <hechavar@gmail.com>
2025-07-20 12:24:50 -0300

    Cleanup

--- lib/ws_server_web/channels/order_status_channel.ex
+++ lib/ws_server_web/channels/order_status_channel.ex
@@ -3,24 +3,22 @@ defmodule WsServerWeb.OrderStatusChannel do
   require Logger
 
   @impl true
-  def join("order_status:lobby", payload, socket) do
-    if authorized?(payload) == true do
-      Logger.debug("Client joined order:status")
-      {:ok, socket}
+  def join("order:" <> order_id, payload, socket) do
+    IO.puts("Order: " <> order_id)
+    IO.inspect(payload)
+    if authorized?(payload) do
+      Logger.debug("Client joined order #{order_id}")
+      {:ok, assign(socket, :order_id, order_id)}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
   @impl true
   def handle_in("ping", payload, socket) do
     {:reply, {:ok, payload}, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (order_status:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
     broadcast(socket, "shout", payload)
